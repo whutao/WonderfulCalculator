da93c020d830ec4ebe875ffd08a8282c2394ff01
Roman <nabroman42@icloud.com>
2021-04-26 00:15:45 +0300

    Connect view controller with storyboard

--- WonderfulCalculator/ViewController.swift
+++ WonderfulCalculator/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    // MARK: - IBOutlets
+    
+    @IBOutlet weak var displayLabel: UILabel!
+    
+    
+    // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    // MARK: - IBActions
+    
+    @IBAction func numberButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func operationButtonPressed(_ sender: UIButton) {
+    }
+    
+    
 }
 
